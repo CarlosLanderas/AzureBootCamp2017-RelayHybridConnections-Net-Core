caae902181be9041c94b3c0e15eea0467eb11baa
unknown <carlos.landeras@gmail.com>
2017-05-31 22:33:23 +0200

    Initial commit

---
+++ azurerelaynode/settings.ts
@@ -0,0 +1,8 @@
+export class AppSettings {
+    public static readonly NAMESPACE = 'coreevent.servicebus.windows.net';
+    public static readonly PATH = 'corehybrid';
+    public static readonly KEYRULE = 'RootManageSharedAccessKey';
+    public static readonly KEY = 'R6fW/SSx2/+4yTpT22RsiaeLtvoSIgxqKvxCraa8ou0=';
+    public static readonly VIEWSFOLDER = '../../views';
+    public static readonly STATICFOLDERS = '../../public';
+}
