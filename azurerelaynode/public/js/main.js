d273bf5db2942b3af8762df1f9f6fa9d6a03435f
unknown <carlos.landeras@gmail.com>
2017-09-24 00:33:07 +0200

    Remove ts generated files

--- azurerelaynode/public/js/main.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var express = require("express");
-var expressServer_1 = require("./server/expressServer");
-var expressApplication = express();
-var port = process.env.PORT || 3000;
-var expressServer = new expressServer_1.ExpressServer(expressApplication, port);
-expressServer.run();
-//# sourceMappingURL=main.js.map
--- azurerelaynode/public/js/server/routes/appRoutes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var relayRouter_1 = require("./relayRouter");
-var routes = [
-    relayRouter_1.default
-];
-exports.routes = routes;
-//# sourceMappingURL=appRoutes.js.map
